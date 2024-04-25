cd9bb03f3afa72fdeb4cd8c753ea26e8884a6e8f
Nguyen Hau <nguyenhau8209>
2024-04-25 20:16:45 +0700

    first commit

---
+++ components/ConnectView.jsx
@@ -0,0 +1,10 @@
+import {W3mButton} from '@web3modal/ethers5-react-native';
+import {ethers} from 'ethers';
+import {useWeb3ModalProvider} from '@web3modal/ethers5-react-native';
+export default function ConnectView() {
+  return (
+    <>
+      <W3mButton />
+    </>
+  );
+}
